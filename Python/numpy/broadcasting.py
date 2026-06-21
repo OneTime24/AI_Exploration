61a4f1d977c1af9f2aca3cd516701e912e0d9b86
OneTime24 <onetimehexa@gmail.com>
2026-06-21 14:56:20 -0400

    new programs

---
+++ Python/numpy/broadcasting.py
@@ -0,0 +1,20 @@
+# Broadcasting =
+
+# How NumPy performs operations on arrays with different shapes.
+
+
+
+import numpy as np
+
+arr1=np.array([1,2,3,4,5,6])
+
+arr1=arr1.reshape(3,2)
+
+
+arr2=np.array([10,10])
+
+print(arr1+arr2)
+
+arr1=arr1*10
+
+print(arr1)
