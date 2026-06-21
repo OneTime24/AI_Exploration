a308ddb4c9ee6267267a83d04aeddf69064eeb16
OneTime24 <onetimehexa@gmail.com>
2026-06-21 13:37:14 -0400

    new program

---
+++ Python/numpy/basic.py
@@ -0,0 +1,37 @@
+
+
+import numpy as np
+
+
+# arr=numpy.array([[10,20,30],
+#  [40,50,60],
+#  [70,80,90]])
+
+# print(arr)
+
+
+# print(arr.shape)
+
+# print(arr[1,1])
+# print(arr[2,2])
+# print(arr[0])
+
+
+# print(arr*10)
+
+
+ar1=np.array([1,2,3])
+
+ar2=np.array([[1,2,3],
+             [4,5,6]
+             ])
+
+
+ar3=np.array([[1],
+             [2],
+             [3]
+             ])
+
+print(ar1.shape)
+print(ar2.shape)
+print(ar3.shape)
