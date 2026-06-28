238313141aa5cadba1d751b16f787a5ed9c3b75a
OneTime24 <onetimehexa@gmail.com>
2026-06-28 04:56:17 -0400

    new stuffs

---
+++ Python/revision_28_7_26.py
@@ -0,0 +1,25 @@
+# basics
+
+
+collec=[{
+    "name":"ali",
+    "age":18,
+    "cgpa":3.5
+},{
+    "name":"ahmed",
+    "age":13,
+    "cgpa":3.4
+},
+{
+    "name":"bilal",
+    "age":20,
+    "cgpa":3.9
+},
+]
+
+
+# print(collec)
+
+
+for nam in collec:
+    print(nam["name"])
