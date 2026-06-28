6904bbb662188e4ba59aa8e4acf94581d68e0600
OneTime24 <onetimehexa@gmail.com>
2026-06-28 08:00:31 -0400

    with new update

--- Python/revision_28_7_26.py
+++ Python/revision_28_7_26.py
@@ -21,5 +21,35 @@ collec=[{
 # print(collec)
 
 
-for nam in collec:
-    print(nam["name"])
+# for nam in collecs
+
+
+
+def sum(*all):
+    total=0;
+
+    for i in all:
+        total+=i
+    
+    return total
+
+
+print(sum(1,2,3,4,5))
+
+
+print(sum(5,4,1))
+
+
+
+stuffs=[
+    {"item":"pen",
+     "price":20,
+     },
+     {"item":"paper",
+      "price":50}
+]
+
+
+# print(stuffs)
+
+for 
