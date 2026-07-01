42d81fe96feef1e184fcbeaf77bcd212acf88cd6
OneTime24 <onetimehexa@gmail.com>
2026-07-01 14:57:21 -0400

    STATIITCS NP

---
+++ Python/numpy/np_statistics.py
@@ -0,0 +1,38 @@
+import numpy as np
+
+# Student Marks
+marks = np.array([78, 92, 45, 67, 89, 92, 56, 45, 81, 73])
+
+print("Student Marks:")
+print(marks)
+
+print("\n----- Statistics -----")
+
+print("Highest Marks:", np.max(marks))
+print("Lowest Marks:", np.min(marks))
+
+print("Average Marks:", np.mean(marks))
+
+print("Total Marks:", np.sum(marks))
+
+print("Standard Deviation:", np.std(marks))
+
+print("Variance:", np.var(marks))
+
+print("\n----- Searching -----")
+
+print("Index of Highest Marks:", np.argmax(marks))
+
+print("Index of Lowest Marks:", np.argmin(marks))
+
+print("\n----- Unique Marks -----")
+
+print(np.unique(marks))
+
+print("\n----- Sorting -----")
+
+print("Sorted Marks:")
+print(np.sort(marks))
+
+print("\nIndices after Sorting:")
+print(np.argsort(marks))
