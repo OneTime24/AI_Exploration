cd744de0f923bc7516ae49664695a04bfa195945
OneTime24 <onetimehexa@gmail.com>
2026-07-01 14:49:58 -0400

    added random nunpy

---
+++ Python/numpy/np_arr.py
@@ -0,0 +1,16 @@
+
+
+import numpy as np
+
+
+
+print(np.random.rand(2,2))
+
+print(np.random.randint(0,10,(3,2)))
+
+print(np.random.randn(5))
+
+print(np.random.seed(42))
+print(np.random.seed(7))
+print(np.random.seed(2020))
+
